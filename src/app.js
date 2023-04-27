918ffe6bf4991ad6b0c2d18ee5fa7e61911fbb10
Obando96 <joseobando30@gmail.com>
2023-04-26 20:53:26 -0500

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const userRouter = require("./users/users.router");
+const db = require("./utils/database")
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//? Validar la conexión
+
+db.authenticate()
+  .then(() => console.log('Database Authenticated!'))
+  .catch((err) => console.log(err))
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server OK",
+    myMessage: process.env.MESSAGE,
+    myPort: process.env.PORT,
+  });
+});
+
+app.use("/", userRouter);
+
+app.listen(3000, () => {
+  console.log(`Server started at port ${PORT}`);
+});
